9d7856c6a8093ee8f43f447d02430bee5e3fbf1d
Marcelo <marcelovqvd@gmail.com>
2022-04-19 18:32:32 -0300

    wip

--- src/routes/Users.routes.js
+++ src/routes/Users.routes.js
@@ -1,9 +1,19 @@
 import { Router } from 'express'
 
+import { User } from '../modules/users/entities/user.js'
+
 const usersRoutes = Router()
 
 usersRoutes.get("/", (request, response) => {
-  return response.status(200).json({msg: 'get'})
+  return response.status(200).json({msg: 'User'})
+})
+
+usersRoutes.post("/", (request, response) => {
+  const { name, email, password } = request.body
+
+  const user = new User(name, email, password)
+
+  return response.status(200).json(user)
 })
 
 export { usersRoutes }
