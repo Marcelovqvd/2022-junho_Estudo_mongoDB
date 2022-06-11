b7a106c0e9deffe8dd343a39d9bd0c93588929b5
Marcelo <marcelovqvd@gmail.com>
2022-06-11 18:21:14 -0300

    .env

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 import express from 'express'
+import dotenv from 'dotenv'
 import { router } from "./routes/index.js";
-
+dotenv.config()
+const port = process.env.PORT || 5000
 const app = express()
+
 
 app.use(express.json())
 app.use(router);
 
-app.listen(3333, () => console.log('Running at 3333'))
+app.listen(port, () => console.log(`Running at ${port}`))
